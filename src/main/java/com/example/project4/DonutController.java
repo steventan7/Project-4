ff4b67fb7d68b5b15526920f9a51eb775c53d1b8
Steven Tan <szt4@scarletmail.rutgers.edu>
2023-03-25 22:18:00 -0400

    Added UI and UX for Donut and Coffee

--- src/main/java/com/example/project4/DonutController.java
+++ src/main/java/com/example/project4/DonutController.java
@@ -1,9 +1,49 @@
 package com.example.project4;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+
 public class DonutController {
     private StoreFrontController mainController;
+    private ObservableList<String> donutList;
+    private ObservableList<String> donutFlavors;
+    private ObservableList<String> donutOrderList;
+    private ObservableList<String> amountList;
+
+    @FXML
+    private ListView<String> donutListView;
+
+    @FXML
+    private ListView<String> donutOrderListView;
+
+    @FXML
+    private ComboBox<String> donutBox;
+
+    @FXML
+    private ComboBox<String> amount;
 
     public void setMainController (StoreFrontController controller){
         mainController = controller;
+    }
+
+    public void initialize() {
+        amountList = FXCollections.observableArrayList("1","2","3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+                "14", "15", "16", "17", "18", "19", "20");
+        donutList = FXCollections.observableArrayList("Yeast Donut", "Cake Donut", "Donut Hole");
+        donutFlavors = FXCollections.observableArrayList("Chocolate", "Vanilla", "Cinnamon", "Apple Cider",
+                "Blueberry", "Pumpkin Spice");
+        donutOrderList = FXCollections.observableArrayList();
+        donutBox.setItems(donutList);
+        donutListView.setItems(donutFlavors);
+        donutOrderListView.setItems(donutOrderList);
+        amount.setItems(amountList);
+    }
+
+    @FXML
+    public void displayFruit() {
+
     }
 }
--- src/main/java/com/example/project4/StoreFrontController.java
+++ src/main/java/com/example/project4/StoreFrontController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
