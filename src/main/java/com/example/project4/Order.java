72d66ddeb051946e44849cee99b2154c7dbadb61
nolipot <david.f2002@outlook.com>
2023-04-03 19:28:21 -0400

    Comments to order

--- src/main/java/com/example/project4/Order.java
+++ src/main/java/com/example/project4/Order.java
@@ -12,15 +12,30 @@ public class Order {
     private ArrayList<MenuItem> listOfMenuItems;
     private int subTotal;
 
+    /**
+     * Constructs an Order instance with its own unique order number and arraylist representing the menu items for
+     * the order.
+     */
     public Order() {
         this.subTotal = 0;
         this.orderNumber = trackingNumber;
         trackingNumber++;
         this.listOfMenuItems = new ArrayList<>();
     }
+
+    /**
+     * Adds the items contained in the argument arraylist to the list of menu items in the order.
+     * @param subOrderList arraylist containing new menu items to add to the order.
+     */
     public void addItems(ArrayList<MenuItem> subOrderList) {
         listOfMenuItems.addAll(subOrderList);
     }
+
+    /**
+     * Removes an item in the order given the item description of the menu item as a String. Traverses through the
+     * list of menu items and compares String values to perform this.
+     * @param itemDesc
+     */
     public void removeItem(String itemDesc) {
         for(MenuItem item : listOfMenuItems) {
             if(item.toString().equals(itemDesc)) {
@@ -30,13 +45,26 @@ public class Order {
         }
     }
 
+    /**
+     * Retrieves the arraylist containing the list of menu items in this order.
+     * @return arraylist with the items in this order.
+     */
     public ArrayList<MenuItem> menuList(){
         return this.listOfMenuItems;
     }
 
+    /**
+     * Retrieves the order number of this order.
+     * @return int representing the order number of this order.
+     */
     public int orderNumber() {
         return this.orderNumber;
     }
+
+    /**
+     * Retrieves the subtotal, before tax, representing the sum of all prices of menu items in this order.
+     * @return double representing the subtotal of this order.
+     */
     public double subTotal() {
         double subtotalAmount = 0.0;
         for(MenuItem item : listOfMenuItems) {
